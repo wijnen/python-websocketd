8686a040bc9e869fff7c05c9f98b3a099b26a336
Bas Wijnen <wijnen@debian.org>
2014-11-07 13:32:23 -0500

    start implementing POST requests for non-websocket connections

--- websockets.py
+++ websockets.py
@@ -44,6 +44,7 @@ if sys.version >= '3':
 	byte = lambda x: bytes((x,))
 	bytelist = lambda x: bytes(x)
 	bord = lambda x: x
+	from http.client import responses as httpcodes
 else:
 	makebytes = lambda x: x
 	makestr = lambda x: x
@@ -52,17 +53,7 @@ else:
 	byte = chr
 	bytelist = lambda x: ''.join([chr(c) for c in x])
 	bord = ord
-# }}}
-
-known_codes = {	# {{{
-		100: 'Continue', 101: 'Switching Protocols',
-		200: 'OK', 201: 'Created', 202: 'Accepted', 203: 'Non-Authorative Information', 204: 'No Content', 205: 'Reset Content', 206: 'Partial Content',
-		300: 'Multiple Choices', 301: 'Moved Permanently', 302: 'Found', 303: 'See Other', 304: 'Not Modified', 305: 'Use Proxy', 307: 'Temporary Redirect',
-		400: 'Bad Request', 401: 'Unauthorized', 402: 'Payment Required', 403: 'Forbidden', 404: 'Not Found',
-			405: 'Method Not Allowed', 406: 'Not Acceptable', 407: 'Proxy Authentication Required', 408: 'Request Timeout', 409: 'Conflict',
-			410: 'Gone', 411: 'Length Required', 412: 'Precondition Failed', 413: 'Request Entity Too Large', 414: 'Request-URI Too Long',
-			415: 'Unsupoported Media Type', 416: 'Requested Range Not Satisfiable', 417: 'Expectation Failed',
-		500: 'Internal Server Error', 501: 'Not Implemented', 502: 'Bad Gateway', 503: 'Service Unavailable', 504: 'Gateway Timeout'}
+	from httplib import responses as httpcodes
 # }}}
 
 class Websocket: # {{{
@@ -88,7 +79,6 @@ Sec-WebSocket-Key: 0\r
 					raise EOFError('EOF while reading reply')
 				hdrdata += r
 			pos = hdrdata.index(b'\n')
-			print(repr(hdrdata))
 			assert int(hdrdata[:pos].split()[1]) == 101
 			hdrdata = hdrdata[pos + 1:]
 			data = {}
@@ -215,7 +205,8 @@ Sec-WebSocket-Key: 0\r
 				self.socket.close()
 				return None
 			self.websocket_fragments += data
-			log('fragment recorded')
+			if DEBUG > 2:
+				log('fragment recorded')
 			return None
 		# Complete frame has been received.
 		data = self.websocket_fragments + data
@@ -480,7 +471,7 @@ class RPC(Websocket): # {{{
 	# }}}
 # }}}
 
-if network.have_glib: # {{{
+if network.have_glib:
 	class Httpd_connection:	# {{{
 		# Internal functions.  {{{
 		def __init__(self, server, socket, httpdirs, websocket = Websocket): # {{{
@@ -501,7 +492,7 @@ if network.have_glib: # {{{
 					self._handle_headers()
 					return
 				key, value = makestr(l).split(':', 1)
-				self.headers[key] = value.strip()
+				self.headers[key.lower()] = value.strip()
 				return
 			else:
 				try:
@@ -514,18 +505,18 @@ if network.have_glib: # {{{
 				return
 		# }}}
 		def _handle_headers(self):	# {{{
-			is_websocket = 'Connection' in self.headers and 'Upgrade' in self.headers and 'Upgrade' in self.headers['Connection'] and 'websocket' in self.headers['Upgrade']
+			is_websocket = 'connection' in self.headers and 'upgrade' in self.headers and 'upgrade' in self.headers['connection'].lower() and 'websocket' in self.headers['upgrade'].lower()
 			self.data = {}
 			msg = self.server.auth_message(self, is_websocket) if callable(self.server.auth_message) else self.server.auth_message
 			if msg:
-				if 'Authorization' not in self.headers:
+				if 'authorization' not in self.headers:
 					self.server.reply(self, 401, headers = {'WWW-Authenticate': 'Basic realm="%s"' % msg.replace('\n', ' ').replace('\r', ' ').replace('"', "'")})
-					if 'Content-Length' not in self.headers or self.headers['Content-Length'].strip() != '0':
+					if 'content-length' not in self.headers or self.headers['content-length'].strip() != '0':
 						self.socket.close()
 					return
 				else:
-					auth = self.headers['Authorization'].split(None, 1)
-					if auth[0] != 'Basic':
+					auth = self.headers['authorization'].split(None, 1)
+					if auth[0].lower() != 'basic':
 						self.server.reply(self, 400)
 						self.socket.close()
 						return
@@ -538,11 +529,29 @@ if network.have_glib: # {{{
 					self.data['password'] = pwdata[1]
 					if not self.server.authenticate(self):
 						self.server.reply(self, 401, headers = {'WWW-Authenticate': 'Basic realm="%s"' % msg.replace('\n', ' ').replace('\r', ' ').replace('"', "'")})
-						if 'Content-Length' not in self.headers or self.headers['Content-Length'].strip() != '0':
+						if 'content-length' not in self.headers or self.headers['content-length'].strip() != '0':
 							self.socket.close()
 						return
 			if not is_websocket:
 				self.body = self.socket.unread()
+				if self.method.upper() == 'POST':
+					if 'content-type' not in self.headers or self.headers['content-type'].lower().split(';')[0].strip() != 'multipart/form-data':
+						log('Invalid Content-Type for POST; must be multipart/form-data (not %s)\n' % (self.headers['content-type'] if 'content-type' in self.headers else 'undefined'))
+						self.server.reply(self, 500)
+						self.socket.close()
+						return
+					args = self._parse_args(self.headers['content-type'])
+					if 'boundary' not in args:
+						log('Invalid Content-Type for POST: missing boundary\n' % (self.headers['content-type'] if 'content-type' in self.headers else 'undefined'))
+						self.server.reply(self, 500)
+						self.socket.close()
+						return
+					self.boundary = '\r\n' + '--' + args['boundary'] + '\r\n'
+					self.endboundary = '\r\n' + '--' + args['boundary'] + '--\r\n'
+					self.post_state = None
+					self.post = {}
+					self.socket.read(self._post)
+					self._post('')
 				try:
 					if not self.server.page(self):
 						self.socket.close()
@@ -554,20 +563,211 @@ if network.have_glib: # {{{
 					self.socket.close()
 				return
 			# Websocket.
-			if self.method != 'GET' or 'Sec-WebSocket-Key' not in self.headers:
+			if self.method.upper() != 'GET' or 'sec-websocket-key' not in self.headers:
 				self.server.reply(self, 400)
 				self.socket.close()
 				return
-			newkey = makestr(base64.b64encode(hashlib.sha1(makebytes(self.headers['Sec-WebSocket-Key'].strip()) + b'258EAFA5-E914-47DA-95CA-C5AB0DC85B11').digest()))
+			newkey = makestr(base64.b64encode(hashlib.sha1(makebytes(self.headers['sec-websocket-key'].strip()) + b'258EAFA5-E914-47DA-95CA-C5AB0DC85B11').digest()))
 			headers = {'Sec-WebSocket-Accept': newkey, 'Connection': 'Upgrade', 'Upgrade': 'websocket', 'Sec-WebSocket-Version': '13'}
 			self.server.reply(self, 101, None, None, headers)
 			self.websocket(None, recv = self.server.recv, url = None, socket = self.socket, mask = (None, False), websockets = self.server.websockets, data = self.data)
+		# }}}
+		def _parse_headers(self, message): # {{{
+			lines = []
+			pos = 0
+			while True:
+				p = message.index(b'\r\n', pos)
+				ln = message[pos:p]
+				pos = p + 2
+				if ln == b'':
+					break
+				if ln[0] in b' \t':
+					if len(lines) == 0:
+						log('header starts with continuation')
+					else:
+						lines[-1] += ln
+				else:
+					lines.append(ln)
+			ret = {}
+			for ln in lines:
+				if ':' not in ln:
+					log('ignoring header line without ":": %s' % ln)
+					continue
+				key, value = [x.strip() for x in ln.split(':', 1)]
+				if key.lower() in ret:
+					log('duplicate key in header: %s' % key)
+				ret[key.lower()] = value
+			return ret, message[pos:]
+		# }}}
+		def _parse_args(self, header): # {{{
+			if b';' not in header:
+				return (header.strip(), {})
+			pos = header.index(b';')
+			main = header[:pos].strip()
+			ret = {}
+			while True:
+				if b'=' not in header[pos:]:
+					log('header argument does not have a value')
+					return main, ret
+				p = header.index(b'=', pos)
+				key = header[pos:p].strip().lower()
+				pos = p + 1
+				value = b''
+				quoted = False
+				while True:
+					first = (len(header), None)
+					if not quoted and b';' in header[pos:]:
+						s = header.index(b';', pos)
+						if s < first:
+							first = (s, ';')
+					if b'"' in header[pos:]:
+						q = header.index(b'"', pos)
+						if q < first:
+							first = (q, '"')
+					if b'\\' in header[pos:]:
+						b = header.index(b'\\', pos)
+						if b < first:
+							first = (b, '\\')
+					value += header[pos:first[0]]
+					pos = first[0] + 1
+					if first[1] == ';':
+						break
+					if first[1] == '\\':
+						value += header[pos]
+						pos += 1
+						continue
+					quoted = not quoted
+				ret[key] = value
+		# }}}
+		def _post(self, data):	# {{{
+			self.body += data
+			if self.post_state is None:
+				# Waiting for first boundary.
+				if self.boundary not in self.body:
+					if self.endboundary in self.body:
+						self._finish_post()
+					return
+				self.body = self.body[self.body.index(self.boundary) + len(self.boundary):]
+				self.post_state = 0
+				# Fall through.
+			while True:
+				if self.post_state == 0:
+					# Reading part headers.
+					if b'\r\n\r\n' not in self.body:
+						return
+					headers, self.body = self._parse_headers(self.body)
+					self.post_state = 1
+					if 'content-type' not in headers:
+						self.post_type = ('text/plain', {'charset': 'us-ascii'})
+					else:
+						self.post_type = self._parse_args(headers['content-type'])
+					if 'content-transfer-encoding' not in headers:
+						self.post_encoding = '7bit'
+					else:
+						self.post_encoding = self._parse_args(headers['content-transfer-encoding'])[0].lower()
+					# Handle decoding of the data.
+					if self.post_encoding == 'base64':
+						self._post_decoder = self._base64_decoder
+					elif self.post_encoding == 'quoted-printable':
+						self._post_decoder = self._quopri_decoder
+					else:
+						self._post_decoder = lambda x, final: (x, '')
+					if 'content-disposition' in headers:
+						args = self._parse_args(headers['content-disposition'])[1]
+						if 'name' in args:
+							name = args['name']
+						else:
+							name = None
+						if 'filename' in args:
+							filename = args['filename']
+						else:
+							filename = None
+					else:
+						name = None
+						filename = None
+					fd, self.post_file = tempfile.mkstemp()
+					self.post_handle = os.fdopen(fd)
+					if name in self.post:
+						os.remove(self.post[name][2])
+					self.post[name] = (headers, filename, self.post_file)
+					# Fall through.
+				if self.post_state == 1:
+					# Reading part body.
+					if self.boundary in self.body:
+						self.post_state = 0
+						rest = self.body[self.body.index(self.boundary) + len(self.boundary):]
+						self.body = self.body[:self.body.index(self.boundary)]
+					if self.endboundary in self.body:
+						self.body = self.body[:self.body.index(self.endboundary)]
+						self.post_state = None
+					decoded, self.body = self._post_decoder(self.body, self.post_state != 1)
+					self.post_handle.write(decoded)
+					if self.post_state == 1:
+						break
+					self.post_handle.close()
+					if self.post_state is None:
+						self._finish_post()
+						return
+					self.body = rest
+		# }}}
+		def _finish_post(self):	# {{{
+			if not self.server.post(self):
+				self.socket.close()
+			for f in self.post:
+				os.remove(self.post[f][2])
+			del self.post
+		# }}}
+		def _base64_decoder(self, data, final):	# {{{
+			ret = b''
+			pos = 0
+			table = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+			current = []
+			while len(data) >= pos + 4 - len(current):
+				c = data[pos]
+				pos += 1
+				if c not in table:
+					if c not in '\r\n':
+						log('ignoring invalid character %s in base64 string' % c)
+					continue
+				current.append(table.index(c))
+				if len(current) == 4:
+					# decode
+					ret += byte(current[0] << 2 | current[1] >> 4)
+					if current[2] != 65:
+						ret += byte(((current[1] << 4) & 0xf0) | current[2] >> 2)
+					if current[3] != 65:
+						ret += byte(((current[2] << 6) & 0xc0) | current[3])
+			return (ret, data[pos:])
+		# }}}
+		def _quopri_decoder(self, data, final):	# {{{
+			ret = b''
+			pos = 0
+			while b'=' in data[pos:-2]:
+				p = data.index(b'=', pos)
+				ret += data[:p]
+				if data[p + 1:p + 3] == '\r\n':
+					ret += '\n'
+					pos = p + 3
+					continue
+				if any(x not in '0123456789ABCDEFabcdef' for x in data[p + 1:p + 3]):
+					log('invalid escaped sequence in quoted printable: %s' % data[p:p + 3])
+					pos = p + 1
+					continue
+				ret += byte(int(data[p + 1:p + 3], 16))
+				pos = p + 3
+			if final:
+				ret += data[pos:]
+				pos = len(data)
+			elif len(pos) >= 2:
+				ret += data[pos:-2]
+				pos = len(data) - 2
+			return (ret, data[pos:])
 		# }}}
 		def _reply_websocket(self, message, content_type):	# {{{
 			m = b''
 			e = 0
 			protocol = 'wss://' if hasattr(self.socket.socket, 'ssl_version') else 'ws://'
-			host = self.headers['Host']
+			host = self.headers['host']
 			for match in re.finditer(self.server.websocket_re, makestr(message)):
 				g = match.groups()
 				if len(g) > 0 and g[0]:
@@ -650,15 +850,15 @@ if network.have_glib: # {{{
 			self.reply(connection, 200, message, 'text/css;charset=utf8')
 		# }}}
 		def reply(self, connection, code, message = None, content_type = None, headers = None):	# Send HTTP status code and headers, and optionally a message.  {{{
-			assert code in known_codes
-			#log('Debug: sending reply %d %s for %s\n' % (code, known_codes[code], connection.address.path))
-			connection.socket.send(makebytes('HTTP/1.1 %d %s\r\n' % (code, known_codes[code])))
+			assert code in httpcodes
+			#log('Debug: sending reply %d %s for %s\n' % (code, httpcodes[code], connection.address.path))
+			connection.socket.send(makebytes('HTTP/1.1 %d %s\r\n' % (code, httpcodes[code])))
 			if headers is None:
 				headers = {}
 			if message is None and code != 101:
 				assert content_type is None
 				content_type = 'text/html;charset=utf-8'
-				message = makebytes('<!DOCTYPE html><html><head><title>%s: %s</title></head><body><h1>%s: %s</h1></body></html>' % (code, known_codes[code], code, known_codes[code]))
+				message = makebytes('<!DOCTYPE html><html><head><title>%s: %s</title></head><body><h1>%s: %s</h1></body></html>' % (code, httpcodes[code], code, httpcodes[code]))
 			if content_type is not None:
 				headers['Content-Type'] = content_type
 				headers['Content-Length'] = len(message)
@@ -669,7 +869,7 @@ if network.have_glib: # {{{
 		# }}}
 		# }}}
 		# If httpdirs is not given, or special handling is desired, this can be overloaded.
-		def page(self, connection):	# A non-WebSocket page was requested.  Use connection.address, connection.method, connection.query, connection.headers and connection.body(which may be incomplete) to find out more.  {{{
+		def page(self, connection):	# A non-WebSocket page was requested.  Use connection.address, connection.method, connection.query, connection.headers and connection.body (which should be empty) to find out more.  {{{
 			if self.httpdirs is None:
 				self.reply(connection, 501)
 				return
@@ -711,8 +911,10 @@ if network.have_glib: # {{{
 					log('no file %s(with supported extension) found in %s' % (base, ', '.join(self.httpdirs)))
 					self.reply(connection, 404)
 					return
-			return self.exts[ext] (connection, open(filename, 'rb').read())
+			return self.exts[ext](connection, open(filename, 'rb').read())
 		# }}}
+		def post(self, connection):	# A non-WebSocket page was requested with POST.  Same as page() above, plus connection.post, which is a dict of name:(headers, sent_filename, local_filename).  When done, the local files are unlinked; remove the items from the dict to prevent this.  The default is not to respond at all (so POST cannot be used to retrieve static pages!)
+			return False
 	# }}}
 	class RPChttpd(Httpd): # {{{
 		class RPCconnection(Httpd_connection):
@@ -722,4 +924,3 @@ if network.have_glib: # {{{
 			Httpd.__init__(self, port, target, RPChttpd.RPCconnection, *a, **ka)
 		# }}}
 	# }}}
-# }}}
